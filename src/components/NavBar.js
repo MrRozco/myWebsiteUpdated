ea427fd260f18469da62313d9751de428c9729b0
MrRozco <bryanorozco1997@hotmail.com>
2024-03-10 23:46:32 -0400

    Make aside menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
 
 function NavBar() {
+
+const [currentSection, setCurrentSection] = useState('home');
+
   return (
-    <div>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/Portfolio">Portfolio</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
-            </ul>
-        </nav>
+    <div className='navbar'>
+        <h1 className="logo">My Portfolio</h1>
+        <div className='menu-items'>
+            <nav>
+                <ul>
+                    <li><Link onClick={()=> setCurrentSection('home')} style={currentSection === 'home' ? {color: 'red'} : {}} to="home" smooth={true} duration={1000}><FontAwesomeIcon icon={["fa-solid, fa-house"]} />Home</Link></li>
+                    <li><Link onClick={()=> setCurrentSection('about')} style={currentSection === 'about' ? {color: 'red'} : {}} to="about" smooth={true} duration={1000}>About</Link></li>
+                    <li><Link onClick={()=> setCurrentSection('portfolio')} style={currentSection === 'portfolio' ? {color: 'red'} : {}} to="portfolio" smooth={true} duration={1000}>Portfolio</Link></li>
+                    <li><Link onClick={()=> setCurrentSection('contact')} style={currentSection === 'contact' ? {color: 'red'} : {}} to="contact" smooth={true} duration={1000}>Contact</Link></li>
+                </ul>
+            </nav>
+        </div>    
     </div>
   )
 }
